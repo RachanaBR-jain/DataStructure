4d8356c6709f1103fe72ee7e509cbb1b64b963d6
RachanaBR-jain <br19rachanajain@gmail.com>
2021-06-03 09:57:50 +0530

    UC1-Lets create a simple Linked List of 56, 30 and 70

---
+++ src/main/java/MyNode.java
@@ -0,0 +1,18 @@
+public class MyNode<K> {
+
+    private K key;
+    private MyNode<K> next;
+
+    public MyNode(K key) {
+        this.key = key;
+        this.next = null;
+    }
+
+    public MyNode<K> getNext() {
+        return next;
+    }
+
+    public void setNext(MyNode<K> next) {
+        this.next = next;
+    }
+}
