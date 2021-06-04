885b6c52626c96a6f52ec31490f0183d4103ec8a
RachanaBR-jain <br19rachanajain@gmail.com>
2021-06-04 12:21:53 +0530

    UC3-Ability to create linkedList of 56->30->70 using using append to end method

--- src/test/java/MyLinkedListTest.java
+++ src/test/java/MyLinkedListTest.java
@@ -17,5 +17,24 @@ public class MyLinkedListTest {
         boolean result=myLinkedList.head.equals(myThirdNode) && myLinkedList.head.getNext().equals(mySecondNode) && myLinkedList.tail.equals(myFirstNode);
         Assertions.assertTrue(result);
     }
+    @Test
+    void given3NumbersWhenAppendedToLinkedListShouldBeAddedToEnd() {
+        MyNode<Integer> myFirstNode = new MyNode<Integer>(56);
+        MyNode<Integer> mySecondNode = new MyNode<Integer>(30);
+        MyNode<Integer> myThirdNode = new MyNode<Integer>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.append(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        myLinkedList.printMyNode();
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assertions.assertTrue(result);
+
+    }
+
+
+
 
 }
