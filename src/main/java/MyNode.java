82390d28961cca43273fb42c871c31d787227e26
RachanaBR-jain <br19rachanajain@gmail.com>
2021-06-04 12:17:40 +0530

    UC2-Ability to create linkedList of 56->30->70 using using add at top

--- src/main/java/MyNode.java
+++ src/main/java/MyNode.java
@@ -1,18 +1,25 @@
-public class MyNode<K> {
-
+public class MyNode<K> implements INode<K> {
     private K key;
-    private MyNode<K> next;
+    private INode next;
 
     public MyNode(K key) {
         this.key = key;
-        this.next = null;
+        this.next = next;
     }
 
-    public MyNode<K> getNext() {
+    public K getKey() {
+        return key;
+    }
+
+    public INode getNext() {
         return next;
     }
 
-    public void setNext(MyNode<K> next) {
-        this.next = next;
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public void setNext(INode next) {
+        this.next = (MyNode<K>)  next;
     }
 }
